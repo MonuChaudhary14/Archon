17a45207976caf106415d3d084549ef1ce2a99d8
MonuChaudhary14 <monuchaudharypoonia@gmail.com>
2026-07-07 03:31:29 +0530

    Updated the LLM model

--- ai_engine/app/core/config.py
+++ ai_engine/app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
 
-    HUGGINGFACEHUB_API_TOKEN: str = ""
+    GROQ_API_KEY: str = ""
     model_config = SettingsConfigDict(
         env_file=".env", 
         env_file_encoding="utf-8", 
--- ai_engine/app/services/llm_service.py
+++ ai_engine/app/services/llm_service.py
@@ -1,22 +1,21 @@
-from langchain_community.llms import HuggingFaceEndpoint
+from langchain_groq import ChatGroq
 from app.core.config import settings
 
 class LLMService:
     def __init__(self):
-        
-        self.llm = HuggingFaceEndpoint(
-            repo_id="mistralai/Mistral-7B-Instruct-v0.2",
-            huggingfacehub_api_token=settings.HUGGINGFACEHUB_API_TOKEN,
-            temperature=0.7
+        self.llm = ChatGroq(
+            model="llama-3.1-8b-instant",
+            temperature=0.7,
+            groq_api_key=settings.GROQ_API_KEY
         )
     
     async def generate_response(self, prompt: str) -> str:
         """Sends a prompt to the LLM and returns the response"""
-        if not settings.HUGGINGFACEHUB_API_TOKEN:
-            return "Error: HUGGINGFACEHUB_API_TOKEN is not set"
+        if not settings.GROQ_API_KEY:
+            return "Error: GROQ_API_KEY is not set"
 
         try:
             response = await self.llm.ainvoke(prompt)
-            return response
+            return response.content
         except Exception as e:
             return f"Failed to generate response: {str(e)}"
